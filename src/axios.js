e85ed782e08b8d44d865d83dc968788de57d0c2b
kaifTruth <kaifm3575@gmail.com>
2025-02-22 03:14:25 +0530

    Added new folder with necessary files

--- src/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-//whenever you arre making rthe req you can append the beginnoing url
-const instance=axios.create({
-    baseURL:"https://api.themoviedb.org/3"
-})
-export default instance;
